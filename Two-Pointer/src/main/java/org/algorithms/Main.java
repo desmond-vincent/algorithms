dc4e92a495d2fceea0ddb6d1660a3be9f55af785
desmondvincent <desmond.j.vincent@gmail.com>
2025-12-09 12:43:56 -0600

    feat: Add two pointer algorithm with the two sum class.

---
+++ Two-Pointer/src/main/java/org/algorithms/Main.java
@@ -0,0 +1,12 @@
+package org.algorithms;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static void main() {
+        int[] arr = {1,2,3,4,5,6};
+        int target = 5;
+        TwoSum twoSum = new TwoSum(arr, target);
+
+    }
+}
---
+++ Two-Pointer/src/main/java/org/algorithms/TwoSum.java
@@ -0,0 +1,28 @@
+package org.algorithms;
+
+public class TwoSum {
+    int [] arr;
+    int target;
+
+    public TwoSum(int[] arr, int target) {
+        this.arr = arr;
+        this.target = target;
+        IO.println(sumTwo(arr, target));
+    }
+
+    private static String sumTwo(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+        while (left < right) {
+            int sum = arr[left] + arr[right];
+            if (sum < target) {
+                left++;
+            } else if (sum > target) {
+                right--;
+            }else {
+                return "Position: " + left + "," + right + " | Value: " + arr[left] + "," + arr[right] + " | Target: " + target;
+            }
+        }
+        return "Target not met";
+    }
+}
